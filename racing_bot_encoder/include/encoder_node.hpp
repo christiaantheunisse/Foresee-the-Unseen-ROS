fde5d39cd014a9da0c9cc6b4e84af67dbd9b0cbd
Christiaan Theunisse <C.Theunisse@student.tudelft.nl>
2024-03-04 09:25:35 +0000

    encoder node now sends custom messages with time stamp

--- racing_bot_encoder/include/encoder_node.hpp
+++ racing_bot_encoder/include/encoder_node.hpp
@@ -1,41 +1,42 @@
-#ifndef ENCODERNODE_H
-#define ENCODERNODE_H
+#include <pigpiod_if2.h>
 
 #include "rclcpp/rclcpp.hpp"
+#include "encoder_sensor.hpp"
 #include "std_msgs/msg/int32.hpp"
 #include "racing_bot_interfaces/msg/encoder_values.hpp"
-#include "encoder_sensor.hpp"
+// #include "racing_bot_interfaces/msg/trajectory.hpp"
 
-namespace racing_bot
-{
-  namespace encoder
-  {
-    /**
-     * @brief Constructs an Encoder Node object with the specified pin configurations.
-     *
-     * This class represents an Encoder Node that reads encoder positions and publishes them as std_msgs::msg::Int32.
-     * The constructor initializes the Encoder Node with the given pin configurations for the left and right encoders.
-     * It sets up the publishers and timer for periodically publishing the encoder positions.
-     *
-     * @param left_pin_a The GPIO pin connected to pin A of the left encoder.
-     * @param left_pin_b The GPIO pin connected to pin B of the left encoder.
-     * @param right_pin_a The GPIO pin connected to pin A of the right encoder.
-     * @param right_pin_b The GPIO pin connected to pin B of the right encoder.
-     */
-    class EncoderNode : public rclcpp::Node
-    {
-    public:
-      EncoderNode(const int left_pin_a, const int left_pin_b, const int right_pin_a, const int right_pin_b);
-      ~EncoderNode();
 
-    private:
-      void publishMessage();
-      rclcpp::Publisher<racing_bot_interfaces::msg::EncoderValues>::SharedPtr encoder_publisher_;
-      rclcpp::TimerBase::SharedPtr publish_timer_;
-      const int pi_handle_;
-      const EncoderSensor left_encoder_;
-      const EncoderSensor right_encoder_;
-    };
-  }
-}
+#ifndef ENCODERNODE_H
+#define ENCODERNODE_H
+namespace racing_bot {
+    namespace encoder {
+        /**
+         * @brief Constructs an Encoder Node object with the specified pin configurations.
+         *
+         * This class represents an Encoder Node that reads encoder positions and publishes them as
+         * std_msgs::msg::Int32. The constructor initializes the Encoder Node with the given pin configurations for the
+         * left and right encoders. It sets up the publishers and timer for periodically publishing the encoder
+         * positions.
+         *
+         * @param left_pin_a The GPIO pin connected to pin A of the left encoder.
+         * @param left_pin_b The GPIO pin connected to pin B of the left encoder.
+         * @param right_pin_a The GPIO pin connected to pin A of the right encoder.
+         * @param right_pin_b The GPIO pin connected to pin B of the right encoder.
+         */
+        class EncoderNode : public rclcpp::Node {
+           public:
+            EncoderNode(const int left_pin_a, const int left_pin_b, const int right_pin_a, const int right_pin_b);
+            ~EncoderNode();
+
+           private:
+            void publishMessage();
+            rclcpp::Publisher<racing_bot_interfaces::msg::EncoderValues>::SharedPtr encoder_publisher_;
+            rclcpp::TimerBase::SharedPtr publish_timer_;
+            const int pi_handle_;
+            const EncoderSensor left_encoder_;
+            const EncoderSensor right_encoder_;
+        };
+    }  // namespace encoder
+}  // namespace racing_bot
 #endif
--- racing_bot_encoder/src/encoder_node.cpp
+++ racing_bot_encoder/src/encoder_node.cpp
@@ -1,42 +1,47 @@
-#include <pigpiod_if2.h>
 #include "encoder_node.hpp"
+
 #include "encoder_constants.hpp"
 
-namespace racing_bot
-{
-  namespace encoder
-  {
-    EncoderNode::EncoderNode(const int left_pin_a, const int left_pin_b, const int right_pin_a, const int right_pin_b)
-        : rclcpp::Node(NODE_NAME),
-          pi_handle_(pigpio_start(NULL, NULL)),
-          left_encoder_(pi_handle_, left_pin_a, left_pin_b),
-          right_encoder_(pi_handle_, right_pin_a, right_pin_b)
-    {
-      encoder_publisher_ = this->create_publisher<racing_bot_interfaces::msg::EncoderValues>(PUBLISHER_TOPIC, WHEEL_QUEUE_SIZE);
-      publish_timer_ = this->create_wall_timer(std::chrono::milliseconds(PUBLISH_RATE), std::bind(&EncoderNode::publishMessage, this));
-    }
 
-    EncoderNode::~EncoderNode()
-    {
-      pigpio_stop(pi_handle_);
-    }
+// Node cannot run without pigpiod ($ sudo pigpiod)
+namespace racing_bot {
+    namespace encoder {
+        EncoderNode::EncoderNode(const int left_pin_a, const int left_pin_b, const int right_pin_a,
+                                 const int right_pin_b)
+            : rclcpp::Node(NODE_NAME),
+              pi_handle_(pigpio_start(NULL, NULL)),
+              left_encoder_(pi_handle_, left_pin_a, left_pin_b),
+              right_encoder_(pi_handle_, right_pin_a, right_pin_b) {
+            
+            encoder_publisher_ =
+                this->create_publisher<racing_bot_interfaces::msg::EncoderValues>(PUBLISHER_TOPIC, WHEEL_QUEUE_SIZE);
+            publish_timer_ = this->create_wall_timer(std::chrono::milliseconds(PUBLISH_RATE),
+                                                     std::bind(&EncoderNode::publishMessage, this));
+        }
 
-    void EncoderNode::publishMessage()
-    {
-      // std_msgs::msg::Int32 right_count, left_count;
-      racing_bot_interfaces::msg::EncoderValues encoder_values;
-      
-      encoder_values.header = std_msgs::msg::Header(this->now())
-      encoder_values.right_encoder = right_encoder_.getPosition();
-      encoder_values.left_encoder = left_encoder_.getPosition();
-      encoder_publisher_.publish(encoder_values);
+        EncoderNode::~EncoderNode() { pigpio_stop(pi_handle_); }
 
-      // left_count.data = left_encoder_.getPosition();
-      // right_count.data = right_encoder_.getPosition();
-      // left_publisher_->publish(left_count);
+        void EncoderNode::publishMessage() {
+            // std_msgs::msg::Int32 right_count, left_count;
 
-      // std_msgs::msg::Int32 right_count;
-      // right_publisher_->publish(right_count);
-    }
-  }
-}
+            racing_bot_interfaces::msg::EncoderValues encoder_values;
+
+            encoder_values.header.stamp = this->now();
+            // encoder_values.header = std_msgs::msg::Header(this->now());
+            std_msgs::msg::Int32 right_reading, left_reading;
+            right_reading.data = right_encoder_.getPosition();
+            left_reading.data = left_encoder_.getPosition();
+
+            encoder_values.right_encoder = right_reading;
+            encoder_values.left_encoder = left_reading;
+            encoder_publisher_->publish(encoder_values);
+
+            // left_count.data = left_encoder_.getPosition();
+            // right_count.data = right_encoder_.getPosition();
+            // left_publisher_->publish(left_count);
+
+            // std_msgs::msg::Int32 right_count;
+            // right_publisher_->publish(right_count);
+        }
+    }  // namespace encoder
+}  // namespace racing_bot
