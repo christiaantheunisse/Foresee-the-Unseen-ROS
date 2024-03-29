33fb8c1677278d72017b60ca7826f244eeb9bfd8
Christiaan Theunisse <C.Theunisse@student.tudelft.nl>
2024-03-29 13:20:20 +0000

    Launch file for robot finished. Map <-> odom transform should be handled on the laptop. Ekf node could not really be debugged yet

--- foresee_the_unseen/launch/bringup_obstacle_launch.py
+++ foresee_the_unseen/launch/bringup_obstacle_launch.py
@@ -1,11 +1,11 @@
 import os
 from launch import LaunchDescription, LaunchContext
 from launch_ros.substitutions import FindPackageShare
-from launch_ros.actions import Node, SetParameter
+from launch_ros.actions import Node, PushRosNamespace
 from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
 from launch.actions.execute_process import ExecuteProcess
 from launch.actions.opaque_function import OpaqueFunction
-from launch.actions import LogInfo
+from launch.actions import LogInfo, GroupAction
 from launch.substitutions import (
     TextSubstitution,
     LaunchConfiguration,
@@ -18,55 +18,6 @@ from launch.substitutions import (
 from launch.conditions import IfCondition, UnlessCondition
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 
-############# Control robot with keyboard ###############
-### $ ros2 run racing_bot_controller teleop_key_node ###
-#########################################################
-
-################ Kill all ROS processes #####################
-### $ kill $(ps aux | grep "ros-args" | awk '{print $2}') ###
-#############################################################
-
-################ Play Ros Bag ##################
-### $ ros2 bag play -l just_on_desk/ --clock ###
-################################################
-
-################ Record Ros Bag ##################
-### $ ros2 bag record -o some_name /scan /wheel_encoders /imu_data ###
-### $ ros2 bag record -o debug_trajectory_mon1 /scan /wheel_encoders /imu_data /trajectory /visualization/trajectory /visualization/planner ###
-##################################################
-
-################################################ Run localization on laptop ###############################################################################################################################################
-### ros2 launch foresee_the_unseen localization_launch.py slam_params_file:=/home/christiaan/thesis/robot_ws/src/foresee_the_unseen/config/mapper_params_localization.yaml map_file_name:=$ROS_MAP_FILES_DIR/on_the_floor ###
-###########################################################################################################################################################################################################################
-
-######## Get tf2_ros tree ##############
-### $ ros2 run tf2_tools view_frames ###
-########################################
-
-########################################## Store a created map ########################################################
-### $ ros2 service call /slam_toolbox/serialize_map slam_toolbox/srv/SerializePoseGraph "{filename: /path/to/file}" ###
-#######################################################################################################################
-
-########################################## Bash script to save a map to a certain location ########################
-### savemap() { # 1 argument with the file_name -> stored in ~/thesis/map_files
-###    filename="$(echo ~/thesis/map_files/$1)"
-###    if test -f "$filename.data"; then
-###        echo "File already exists! Map is not saved."
-###    else
-###        echo "Saving map to $filename ..."
-###        ros2 service call /slam_toolbox/serialize_map slam_toolbox/srv/SerializePoseGraph "{filename: $filename}"
-###    fi
-### }
-#######################################################################################################################
-
-### If defined, the maps are obtained from `ROS_MAP_FILES_DIR`
-### Logs are stored in  `ROS_LOG_FILES_DIR`
-
-
-# TODO: Separate launch file for the bringing up of the real robot.
-
-# ros2 run  datmo2 datmo_node --ros-args -p use_sim_time:=true -p pub_markers:=true
-
 
 def generate_launch_description():
     namespace_launch_arg = DeclareLaunchArgument(
@@ -74,7 +25,21 @@ def generate_launch_description():
         default_value=TextSubstitution(text="obstacle"),
         description="The namespace used for the maps and all the topics.",
     )
+    use_ekf_launch_arg = DeclareLaunchArgument(
+        "use_ekf",
+        default_value=TextSubstitution(text="true"),
+        description="Use the extended kalman filter for the odometry data."
+        "This will also activate and use the imu if available.",
+    )
+    lidar_reverse_launch_arg = DeclareLaunchArgument(
+        "lidar_reverse",
+        default_value=TextSubstitution(text="false"),
+        description="True if the lidar is mounted with the motor facing forward",
+    )
+
     namespace = LaunchConfiguration("namespace")
+    use_ekf = LaunchConfiguration("use_ekf")
+    lidar_reverse = LaunchConfiguration("lidar_reverse")
 
     # To start the `pigpiod package`, necessary for I2C
     start_pigpiod = ExecuteProcess(
@@ -85,13 +50,13 @@ def generate_launch_description():
     hat_node = Node(
         package="racing_bot_hat",
         executable="hat_node",
-        namespace=namespace,
         parameters=[
             {
                 "left_reverse": True,
                 "right_reverse": True,
             }
         ],
+        namespace=namespace,
     )
     encoder_node = Node(
         package="racing_bot_encoder",
@@ -114,6 +79,7 @@ def generate_launch_description():
     )
 
     def namespace_as_string(context: LaunchContext, namespace: LaunchConfiguration):
+        """In this function the namespace is available as string and can be used for the frames"""
         namespace_str = namespace.perform(context)
 
         odometry_node = Node(
@@ -131,7 +97,6 @@ def generate_launch_description():
             ],
             namespace=namespace,
         )
-
         trajectory_node = Node(
             package="racing_bot_trajectory_follower",
             executable="trajectory_follower_node",
@@ -148,16 +113,69 @@ def generate_launch_description():
             ],
             namespace=namespace,
         )
+        ekf_node = Node(
+            package="robot_localization",
+            executable="ekf_node",
+            name="ekf_node",
+            parameters=[
+                PathJoinSubstitution([FindPackageShare("foresee_the_unseen"), "config", "ekf_obstacle.yaml"]),
+                {
+                    "odom_frame": f"{namespace_str}/odom",
+                    "base_link_frame": f"{namespace_str}/base_link",
+                },
+            ],
+            condition=IfCondition(use_ekf),  # use_ekf
+            namespace=namespace,
+        )
+        lidar_launch = GroupAction(
+            actions=[
+                PushRosNamespace(namespace),
+                IncludeLaunchDescription(
+                    PythonLaunchDescriptionSource(
+                        PathJoinSubstitution([FindPackageShare("sllidar_ros2"), "launch", "sllidar_a1_launch.py"])
+                    ),
+                    launch_arguments=[("frame_id", f"{namespace_str}/laser")],
+                ),
+            ]
+        )
+
+        # Needed: transform from map to odom: SLAM or static, but should be handle on the laptop because it is based on
+        #  the trajectory for the obstacle
+
+        static_trans_base_link_to_laser = Node(
+            package="tf2_ros",
+            executable="static_transform_publisher",
+            arguments=(
+                f"--x 0 --y 0 --z 0 --roll 0 --pitch 0 --yaw 3.14 "
+                + f"--frame-id {namespace_str}/base_link --child-frame-id {namespace_str}/laser"
+            ).split(" "),
+            condition=UnlessCondition(lidar_reverse),
+        )
+        static_trans_base_link_to_laser_reverse = Node(
+            package="tf2_ros",
+            executable="static_transform_publisher",
+            arguments=(
+                f"--x 0 --y 0 --z 0 --roll 0 --pitch 0 --yaw 0 "
+                + f"--frame-id {namespace_str}/base_link --child-frame-id {namespace_str}/laser"
+            ).split(" "),
+            condition=IfCondition(lidar_reverse),
+        )
 
         return [
             odometry_node,
             trajectory_node,
+            ekf_node,
+            lidar_launch,
+            static_trans_base_link_to_laser,
+            static_trans_base_link_to_laser_reverse,
         ]
 
     return LaunchDescription(
         [
             # arguments
             namespace_launch_arg,
+            use_ekf_launch_arg,
+            lidar_reverse_launch_arg,
             # commands
             start_pigpiod,
             # nodes
